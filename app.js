f8fb5e3a246c9984d4311da6a4d6e9177b7c9c98
sujeetRawat <sujeetrawat890@gmail.com>
2024-08-30 22:07:57 +0530

    Updated - added contact us route

--- app.js
+++ app.js
@@ -1,18 +1,26 @@
 const express = require('express');
+
+const path = require('path');
+
 const bodyParser= require('body-parser');
+
 const shopRoutes = require('./routes/shop');
 const adminRoutes = require('./routes/admin');
+const contactUsRoutes = require('./routes/contact');
 
 const app= express(); 
 
 app.use(bodyParser.urlencoded({extended : false}));
 
+app.use(express.static(path.join(__dirname,'public')));
+
 app.use('/admin',adminRoutes);
 app.use('/admin',shopRoutes);
+app.use('/admin',contactUsRoutes);
 
 
 app.use((req,res) => {
-    res.status(404).send('<h1>Page not Found !! </h1>');
+    res.sendFile(path.join(__dirname,'views','Error.html'));
 })
 
 app.listen(3000);        
