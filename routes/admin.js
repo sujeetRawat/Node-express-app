9c574ac20214c5c4514ed0ab76a8e0ca8a2c442d
sujeetRawat <sujeetrawat890@gmail.com>
2024-08-31 01:47:58 +0530

    Updated , Forgot to add in the previous commit

--- routes/admin.js
+++ routes/admin.js
@@ -1,16 +1,18 @@
 const express = require('express');
 
+const path = require('path');
 
 const router =express.Router();
 
+const rootDir = require('../util/path');
 
-router.get('/add-product', (req,res) => {
-    res.send('<form action="/admin/product" method="POST">Name : <input type="text" name="product"></input>Size : <input type="number" name="size"></input><button>Add product</button></form>')
-})
+router.get('/add-product', (req,res) =>{
+    res.sendFile(path.join(rootDir,'views','add-product.html'));
+});
 
-router.post('/product',(req,res)=>{
-    console.log(req.body);
-    res.redirect('/admin');
-})
+router.post('/add-product',(req,res) =>{
+    res.sendFile(path.join(rootDir,'views','add-product.html'));
+    res.redirect('/');
+});
 
 module.exports= router;
--- routes/shop.js
+++ routes/shop.js
@@ -1,9 +1,13 @@
 const express = require('express');
 
+const path = require('path');
+
 const router = express.Router();
 
+const rootDir = require('../util/path');
+
 router.get('/',(req,res) =>{
-    res.send('<h1>Hello from express</h1>');
+    res.sendFile(path.join(rootDir,'views','shop.html'));
 });
 
 module.exports = router;
